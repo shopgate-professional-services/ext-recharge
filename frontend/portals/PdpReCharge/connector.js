b9dfd0dbb67b8e02649601a760cc9fef362c5cf8
Carsten Schneider <carsten.schneider@shopgate.com>
2019-08-02 14:42:19 +0200

    CCP-1635 created mock pipeline

---
+++ frontend/portals/PdpReCharge/connector.js
@@ -0,0 +1,11 @@
+import { connect } from 'react-redux';
+
+const mapStateToProps = dispatch => ({
+  
+});
+
+const mapDispatchToProps = dispatch => ({
+  
+});
+
+export default connect(mapStateToProps, mapDispatchToProps);
