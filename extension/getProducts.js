09a03bfceb91e77c0727fe0ba2020ae7788b3168
Aaron Martin <aaron.martin@shopgate.com>
2019-08-02 13:39:18 -0500

    CCP-1635: added api client and added handling for empty productIds

--- extension/getProducts.js
+++ extension/getProducts.js
@@ -1,28 +1,11 @@
+const ReChargeApi = require('./utilities/ReChargeApi')
+
 module.exports = async (context, input) => {
-  const { apiToken } = context.config;
-
-  // TODO: check if input.productIds is !empty
-
-  const request = context.tracedRequest('ShopgateProjectReChargeProducts');
-
-  // TODO: refactor to general API client later
-
-  const params = {
-    uri: 'https://api.rechargeapps.com/products',
-    qs: {
-      shopify_product_ids: input.productIds,
-    },
-    method: 'GET',
-    headers: {
-      'X-Recharge-Access-Token': apiToken,
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  };
-
-  const response = await request(params);
-  // TODO: error handling
-  console.warn(response);
-
-  return {products: JSON.parse(response).products};
+  const { productIds = [] } = input || {}
+  if (productIds.length < 1) {
+    return { products: [] }
+  }
+  const api = new ReChargeApi(context)
+  const products = await api.getProducts(input.productIds)
+  return { products }
 }
