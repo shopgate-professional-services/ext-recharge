445fdc17305cf0e61781d113c621437f357d7ecb
Daniel Quintanilla <daniel.quintanilla@shopgate.com>
2019-08-23 12:22:14 -0500

    CCP-1669: update

--- extension/lib/buildRechargeCart.js
+++ extension/lib/buildRechargeCart.js
@@ -15,14 +15,18 @@ module.exports = async function buildRechargeCart (context, input) {
 
   const tax = input.totals.find(({ type }) => type === 'tax')
   const discounts = input.totals.filter(({ type }) => type === 'discount')
-  let totalAmount = input.totals.find(({ type }) => type === 'grandTotal').amount
+  let grandTotal = input.totals.find(({ type }) => type === 'grandTotal').amount
+  let discounted = false
 
-  const items = cartItems.map((cartItem) => {
+  const items = []
+  cartItems.forEach((cartItem) => {
     const product = cartItem.product
     const rechargeInfo = product.additionalInfo.find(({ recharge }) => recharge) || null
     const { shopifyVariantId } = product.additionalInfo.find(({ shopifyVariantId }) => shopifyVariantId) || {}
+    // Should be used to add non recharge applicable products - need the shopfiy variant_id to create a recharge checkout line item
     if (!rechargeInfo) {
-      return {
+      items.push({
+        order_reference: cartItem.id,
         name: product.name,
         reference: cartItem.id,
         total_amount: product.price.unit * cartItem.quantity,
@@ -31,46 +35,68 @@ module.exports = async function buildRechargeCart (context, input) {
         image_url: product.featuredImageUrl ? product.featuredImageUrl : undefined,
         properties: product.properties.map(({ label: key, value }) => ({ key, value })),
         shopifyVariantId
-      }
+      })
+      return
     }
 
-    const { subscriptionInfo } = rechargeInfo.recharge[0]
-    const discountPercentage = subscriptionInfo.discountAmount || null
+    // should be used to add recharge applicable items based on selected or non selected subscription.
+    const mappedSubscriptions = rechargeInfo.recharge.map((info) => {
+      const { subscriptionInfo } = info || null
+      const { frequencyValue } = info || null
+      const { shopifyVariantId } = subscriptionInfo || null
 
-    if (discountPercentage) {
-      const deductedPrice = product.price.unit - (product.price.unit * (discountPercentage / 100))
-      totalAmount = updateTotalAmount(totalAmount, product.price.unit, deductedPrice)
+      if (frequencyValue === 'No Subscription') {
+        return {
+          order_reference: cartItem.id,
+          name: product.name,
+          reference: cartItem.id,
+          total_amount: product.price.unit * subscriptionInfo.quantity,
+          unit_price: product.price.unit,
+          quantity: subscriptionInfo.quantity,
+          image_url: product.featuredImageUrl ? product.featuredImageUrl : undefined,
+          properties: product.properties.map(({ label: key, value }) => ({ key, value })),
+          shopifyVariantId
+        }
+      }
+
+      const discountPercentage = subscriptionInfo.discountAmount || null
+      if (discountPercentage) {
+        const deductedPrice = product.price.unit - (product.price.unit * (discountPercentage / 100))
+
+        discounted = true
+
+        return {
+          name: product.name,
+          reference: cartItem.id,
+          total_amount: deductedPrice * subscriptionInfo.quantity,
+          unit_price: deductedPrice,
+          quantity: subscriptionInfo.quantity,
+          image_url: product.featuredImageUrl ? product.featuredImageUrl : undefined,
+          properties: product.properties.map(({ label: key, value }) => ({ key, value })),
+          shopifyVariantId,
+          subscriptionInfo
+        }
+      }
 
       return {
         name: product.name,
         reference: cartItem.id,
-        total_amount: deductedPrice * cartItem.quantity,
-        unit_price: deductedPrice,
-        quantity: cartItem.quantity,
+        total_amount: product.price.unit * subscriptionInfo.quantity,
+        unit_price: product.price.unit,
+        quantity: subscriptionInfo.quantity,
         image_url: product.featuredImageUrl ? product.featuredImageUrl : undefined,
         properties: product.properties.map(({ label: key, value }) => ({ key, value })),
         shopifyVariantId,
         subscriptionInfo
       }
-    }
+    })
 
-    return {
-      name: product.name,
-      reference: cartItem.id,
-      total_amount: product.price.unit * cartItem.quantity,
-      unit_price: product.price.unit,
-      quantity: cartItem.quantity,
-      image_url: product.featuredImageUrl ? product.featuredImageUrl : undefined,
-      properties: product.properties.map(({ label: key, value }) => ({ key, value })),
-      shopifyVariantId,
-      subscriptionInfo
-    }
+    items.push(...mappedSubscriptions)
   })
 
   const cart = {
-    order_reference: input.cartId,
     currency: input.currency,
-    total_amount: totalAmount,
+    total_amount: items.reduce((total, item) => item.total_amount + total, 0),
     tax_amount: tax ? tax.amount * 100 : undefined,
     items,
     discounts: discounts.map((d) => ({
@@ -81,5 +107,3 @@ module.exports = async function buildRechargeCart (context, input) {
 
   return { cart }
 }
-
-const updateTotalAmount = (totalAmount, price, deductedPrice) => ((totalAmount - price) + deductedPrice)
