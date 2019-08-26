515288a1f4b59327fe0e0aa5955aa0e4980d6a20
Aaron Martin <aaron.martin@shopgate.com>
2019-08-25 19:54:05 -0500

    CCP-1659: adjusted recharge option labels and added default no subscription to products with an optional subscription

--- frontend/components/RechargeOption/index.jsx
+++ frontend/components/RechargeOption/index.jsx
@@ -6,13 +6,10 @@ import { SheetDrawer, SheetList } from '@shopgate/engage/components';
 import SheetItem from './components/SheetItem';
 import {
   REQUIRED_SUBSCRIPTION_TEXT,
-  DISCOUNT_TYPE_PERCENTAGE,
+  NO_SUBSCRIPTION_FREQUENCY_VALUE,
 } from '../../constants';
 import transition from './transition';
 import styles from './style';
-import getConfig from '../../helpers/getConfig';
-
-const { rechargeCurrency } = getConfig();
 
 /**
  * @returns {JSX}
@@ -38,10 +35,10 @@ const RechargeOption = ({
   const [showSheet, setShowSheet] = useState(false);
   const [selected, setSelected] = useState(null);
   const [highlight, setHighlight] = useState(false);
-
+  const isSubscriptionOptional = purchaseOption !== REQUIRED_SUBSCRIPTION_TEXT;
   // ComponentWillUnmount - reset selected to null
   useEffect(() => {
-    setSelected(null);
+    setSelected(isSubscriptionOptional ? NO_SUBSCRIPTION_FREQUENCY_VALUE : null);
     const currentlySelectedFrequency = null;
     updateRechargeInfo(currentlySelectedFrequency, selectedSubscriptionsInfo);
   }, []);
@@ -128,35 +125,18 @@ const RechargeOption = ({
     handleClose();
   };
 
-  const isSubscriptionOptional = purchaseOption !== REQUIRED_SUBSCRIPTION_TEXT;
+  const optionLabel = `${i18n.text('recharge.subscription_option.subscription')}${isSubscriptionOptional ? ` (${i18n.text('recharge.subscription_option.optional')})` : ''}`;
 
   /**
-   * Generate Option Label
-   * @return {string}
-   */
-  const generateOptionLabel = () => {
-    const intro = i18n.text('recharge.subscription_option.subscription');
-    let save = '';
-    if (discountAmount > 0) {
-      const discount = discountType === DISCOUNT_TYPE_PERCENTAGE
-        ? ` ${discountAmount}%`
-        : ` ${i18n.price(discountAmount / 100, rechargeCurrency, 2)}`;
-      save = ` ${i18n.text('recharge.subscription_option.and_save')} ${discount}`;
-    }
-    const optionalText = isSubscriptionOptional
-      ? ` (${i18n.text('recharge.subscription_option.optional')})` : '';
-
-    return `${intro}${save}${optionalText}`;
-  };
-
-  const optionLabel = generateOptionLabel();
-  /**
-   * @param {string} defaultLabel default recharge label
    * @returns {string}
    */
-  const getButtonLabel = (defaultLabel) => {
-    if (!selected || selected === 'No Subscription') {
-      return defaultLabel;
+  const getButtonLabel = () => {
+    if (!selected) {
+      return optionLabel;
+    }
+
+    if (selected === NO_SUBSCRIPTION_FREQUENCY_VALUE) {
+      return i18n.text('recharge.subscription_option.no_subscription');
     }
 
     const valueLabel = frequencyValues.find(frequencyValue => frequencyValue === selected);
@@ -170,7 +150,7 @@ const RechargeOption = ({
   * @returns {JSX}
   */
   const transitionRenderer = (state) => {
-    const buttonLabel = getButtonLabel(optionLabel);
+    const buttonLabel = getButtonLabel();
 
     return (
       <div
@@ -181,7 +161,7 @@ const RechargeOption = ({
         onClick={handleOpen}
         style={transition[state]}
       >
-        {selected && selected !== 'No Subscription' && <div className={styles.label}>{optionLabel}</div>}
+        {selected && <div className={styles.label}>{optionLabel}</div>}
         <div className={styles.selection}>{buttonLabel}</div>
       </div>
     );
