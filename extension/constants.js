a629916b074194327061461c507ae8a5f3252ae4
Aaron Martin <aaron.martin@shopgate.com>
2019-08-27 16:27:15 -0500

    CCP-1675: added step hook to prevent updates to cart items with subscriptions

--- extension/constants.js
+++ extension/constants.js
@@ -1,4 +1,5 @@
 module.exports = Object.freeze({
   RECHARGE_INFO_KEY: 'rechargeSubscriptionInfo',
-  RECHARGE_MIRROR_KEY: 'rechargeMirrorCart'
+  RECHARGE_MIRROR_KEY: 'rechargeMirrorCart',
+  RECHARGE_LINE_ITEM_ID_TO_PRODUCT_MAP: 'recharge_line_item_id_to_product_map'
 })
